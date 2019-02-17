3dda2661d647da33feb3207a3ae890710461c696
Lazilyleaf <43440669+Lazilyleaf@users.noreply.github.com>
2019-02-17 18:24:54 +0800

    Update password.php

--- view/nexmoe/password.php
+++ view/nexmoe/password.php
@@ -4,7 +4,7 @@
 	
 <div class="mdui-container-fluid">
 	<div class="mdui-col-md-6 mdui-col-offset-md-3">
-	  <center><h1 class="mdui-typo-display-2-opacity">输入密码进行查看</h1></center>
+	  <center><h1 class="mdui-typo-display-2-opacity">您访问的文件已被加密保护，请输入密码进入</h1></center>
 	  <form action="" method="post">
 		  <div class="mdui-textfield mdui-textfield-floating-label">
 		    <i class="mdui-icon material-icons">https</i>
@@ -14,7 +14,7 @@
 		  <br>
 		  <button type="submit" class="mdui-center mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme">
 		  	<i class="mdui-icon material-icons">fingerprint</i>
-		  	查看
+		  	确定
 		  </button>
 	  </form>
 	</div>
