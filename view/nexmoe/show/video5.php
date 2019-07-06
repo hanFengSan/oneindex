0558964008482cdc748882eb477c8acc2d06315b
yakami <c360785655@gmail.com>
2019-07-06 21:01:51 +0800

    update

--- view/nexmoe/show/video5.php
+++ view/nexmoe/show/video5.php
@@ -16,7 +16,7 @@ $item['thumb'] = onedrive::thumbnail($item['path']);
 	    <option value="<?php e($url);?>" selected>下载地址</option>
 	    <option value="<video><source src='<?php e($url);?>' type='video/mp4'></video>">引用地址</option>
 	  </select>
-    <a href="javascript:copyToClipboard('<?php e($url);?>')">复制链接</a>
+    <a href="javascript:window.copyToClipboard('<?php e($url);?>')" style="text-decoration: none;color: white;background: #1e89f2;padding: 6px 8px;border-radius: 2px;position: relative;top: 3px;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.16), 0 2px 4px rgba(0, 0, 0, 0.23);left: 20px;">复制链接</a>
 	  <textarea class="mdui-textfield-input" id="val" readonly><?php e($url);?></textarea>
 	</div>
 	<script type="text/javascript">
